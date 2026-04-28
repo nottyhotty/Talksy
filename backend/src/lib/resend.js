0af43ed0677b48766465b9739e21c51b86794241
Gautam Banjara <gautambanjara2005@gmail.com>
2026-04-28 12:46:30 +0530

    Initial project upload

--- backend/src/lib/resend.js
+++ backend/src/lib/resend.js
@@ -1,7 +1,7 @@
 import {Resend} from'resend';
 import 'dotenv/config';
 
-export const Resendclient = new Resend(process.env.RESEND_API_KEY);
+export const resendClient = new Resend(process.env.RESEND_API_KEY);
 
 export const sender={
     email:process.env.EMAIL_FROM,
